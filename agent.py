dd13e7cf73f85ee38b79e991aa8dca54fd721b0a
Akash-Rana2 <akashvrana@gmail.com>
2025-07-22 23:17:52 +0530

    added transcription

--- agent.py
+++ agent.py
@@ -1,10 +1,15 @@
 from dotenv import load_dotenv
 from livekit import agents
-from livekit.agents import AgentSession, Agent, RoomInputOptions
-from livekit.plugins import (
-    openai,
-    noise_cancellation,
+from livekit.agents import (AgentSession, Agent, RoomInputOptions,RoomOutputOptions,function_tool,)
+from livekit import rtc
+from livekit.plugins.google import (
+   LLM
 )
+from livekit.plugins import noise_cancellation,deepgram,cartesia
+import os
+import datetime
+
+import asyncio
 from prompts import WELCOME_PROMPT, ROOM_TYPES_INFO
 from api import (
     search_available_rooms,
@@ -17,7 +22,7 @@ from api import (
     get_booking_summary
 )
 
-load_dotenv()
+load_dotenv("env_example.env")
 
 
 class HotelReceptionistAgent(Agent):
@@ -30,20 +35,38 @@ class HotelReceptionistAgent(Agent):
                 get_room_pricing,
                 book_room,
                 get_room_details,
-                suggest_room_for_occasion,
+                suggest_room_for_occasion, 
                 calculate_discount,
-                get_booking_summary
+                get_booking_summary,
             ]
         )
+
+    
+
 
 
 async def entrypoint(ctx: agents.JobContext):
     session = AgentSession(
-        llm=openai.realtime.RealtimeModel(
-            voice="alloy"
-        )
+        stt=deepgram.STT(
+            api_key="afbc8893b9e4f291f1f2a24b4aef77524e35047e",
+            
+        ),
+        llm= LLM(
+            model="gemini-2.5-flash-preview-04-17",
+            api_key="AIzaSyBaSOX01gr8YMGIR_6UCtnPH1tCXAqYxfo"
+        ),
+        tts=cartesia.TTS(
+            api_key="sk_car_2Gq9PKQuwL7smGS8ZFfi9V",
+            voice="en-US-Wavenet-D"
+        ),
     )
 
+    @session.on("user_input_transcribed")
+    def on_transcript(transcript):
+        if transcript.is_final:
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            with open("user_speech_log.txt", "a") as f:
+                f.write(f"[{timestamp}] {transcript.transcript}\n") 
     await session.start(
         room=ctx.room,
         agent=HotelReceptionistAgent(),
@@ -53,9 +76,16 @@ async def entrypoint(ctx: agents.JobContext):
             # - For telephony applications, use `BVCTelephony` for best results
             noise_cancellation=noise_cancellation.BVC(),
         ),
+        room_output_options=RoomOutputOptions(
+            # LiveKit Cloud enhanced noise cancellation
+            # - If self-hosting, omit this parameter
+            # - For telephony applications, use `BVCTelephony` for best results
+            transcription_enabled=True
+        )
     )
-
-    await ctx.connect()
+   
+          
+    await ctx.connect(auto_subscribe=True)
 
     await session.generate_reply(
         instructions="Greet the user warmly as a hotel receptionist and offer to help them with room reservations. Mention that you can help them find the perfect room, check availability, and provide special discounts for special occasions."
